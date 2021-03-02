1a58055b9388034cd17b7431530340fe5daedc30
ha-ma <01yhamaya05@gmail.com>
2021-03-02 23:59:59 +0900

    add link

--- components/BottomContact.js
+++ components/BottomContact.js
@@ -6,7 +6,7 @@ export default function ButtomContact() {
     <section className={styles.bottomContact}>
       <h2 className={styles.bottomContact__heading}>お問い合わせ</h2>
       <p className={styles.bottomContact__desc}>取材依頼・公園・ワークショップの企画のご相談などお気軽にご連絡ください。</p>
-      {/* <Link to="/contact" className={styles.bottomContact__link}>お問い合わせはこちら</Link> */}
+      <Link href="/contact" className={styles.bottomContact__link}>お問い合わせはこちら</Link>
     </section>
   )
 }
