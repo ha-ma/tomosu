400759daf04597905f6871ed239590e15091dc03
sasa <mwa00394@gmail.com>
2021-03-03 18:19:25 +0900

    共通お問い合わせ作成

--- components/BottomContact.js
+++ components/BottomContact.js
@@ -4,9 +4,12 @@ import styles from './BottomContact.module.scss'
 export default function ButtomContact() {
   return (
     <section className={styles.bottomContact}>
-      <h2 className={styles.bottomContact__heading}>お問い合わせ</h2>
-      <p className={styles.bottomContact__desc}>取材依頼・公園・ワークショップの企画のご相談などお気軽にご連絡ください。</p>
-      <Link href="/contact" className={styles.bottomContact__link}>お問い合わせはこちら</Link>
+      <Link href="/contact">
+        <a className={styles.bottomContact__link}>
+          <h2 className={styles.bottomContact__heading}>お問い合わせ</h2>
+          <p className={styles.bottomContact__desc}>取材依頼・講演・ワークショップの企画の<br />ご相談などお気軽にご連絡ください。</p>
+        </a>
+      </Link>
     </section>
   )
 }
