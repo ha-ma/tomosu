3ac84c9cb6a3924ace9287f4dbdf0b6e02902afa
sasa <mwa00394@gmail.com>
2021-03-03 15:26:32 +0900

    Layout.js, Global.jsのスタイル追加、footer作成

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/Layout"
 import BottomContact from '../components/BottomContact'
 import BottomSNS from '../components/BottomSNS'
 import Footer from '../components/Footer'
+import '../styles/reset.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
