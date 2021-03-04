639f6d1e89a409e46f251b76c772a3d73109d136
sasa <mwa00394@gmail.com>
2021-03-04 16:58:32 +0900

    PageHero作成

---
+++ pages/sdgs/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import styles from '../styles/sdgs.module.scss'
+import Image from 'next/image'
+import PageHero from '../../components/PageHero'
+
+function Sdgs() {
+  return (
+    <>
+      <PageHero />
+      <section>
+        sdgs
+      </section>
+    </>
+  )
+}
+
+export default Sdgs
