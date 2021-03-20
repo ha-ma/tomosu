c57417cf620528f4a2f1317b6ec171fa2788ac08
Yuta Hamaya <01yhamaya05@gmail.com>
2021-03-20 10:59:16 +0900

    Articlesページをアップデート、画像情報取得のためにconfigファイル追加

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import Prismic from "@prismicio/client";
-// import { Client } from "../../prismic-configuration";
+import Prismic from "@prismicio/client";
+import { Client } from "../../prismic-configuration";
 import styles from '../../styles/articles.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -8,7 +8,7 @@ import PageHero from '../../components/PageHero'
 import Pagination from '../../components/Pagination'
 
 const Artiles = ({ articles }) => {
-  // console.log("articles", articles);
+  console.log("articles", articles);
   return (
     <>
       <PageHero />
@@ -20,174 +20,39 @@ const Artiles = ({ articles }) => {
           </div>
           <div className={styles.articleslist__listBlock}>
             <ul className={styles.articleslist__list}>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
+
+              {
+                articles.map(article => (
+                  <li className={styles.articleslist__item}>
+                    <Link href="/articles">
+                      <a className={styles.articleslist__link}>
+                        <div className={styles.articleslist__img}>
+                          {
+                            article.data.eyecatch.url
+                            ?
+                            <Image src={article.data.eyecatch.url} quality={100} width={600} height={400} />
+                            :
+                            <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
+                          }
+                        </div>
+                        <ul className={styles.articleslist__category}>
+                          <li className={styles.articleslist__category__item}>セクション名</li>
+                        </ul>
+                        <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
+                      </a>
+                    </Link>
+                    <ul className={styles.articleslist__tag}>
+                      <li className={styles.articleslist__tag__item}>
+                        <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
+                      </li>
+                      <li className={styles.articleslist__tag__item}>
+                        <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
+                      </li>
                     </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
                   </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
-              <li className={styles.articleslist__item}>
-                <Link href="/articles">
-                  <a className={styles.articleslist__link}>
-                    <div className={styles.articleslist__img}>
-                      <Image src="/images/sample_article_image.png" quality={100} width={600} height={400} />
-                    </div>
-                    <ul className={styles.articleslist__category}>
-                      <li className={styles.articleslist__category__item}>セクション名</li>
-                    </ul>
-                    <h3 className={styles.articleslist__item__heading}>タイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入りますタイトルが入ります</h3>
-                  </a>
-                </Link>
-                <ul className={styles.articleslist__tag}>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                  <li className={styles.articleslist__tag__item}>
-                    <Link href="/"><a className={styles.articleslist__tag__link}>#ハッシュタグ</a></Link>
-                  </li>
-                </ul>
-              </li>
+                ))
+              }
+
             </ul>
             <Pagination />
           </div>
@@ -197,19 +62,20 @@ const Artiles = ({ articles }) => {
   );
 };
 
-// export async function getStaticProps({ params }) {
-//   const client = Client();
-//   // const doc = await client.getByUID("page", params.uid);
+export async function getStaticProps({ params }) {
+  const client = Client();
+  // const doc = await client.getByUID("page", params.uid);
 
-//   const articles = await client.query(Prismic.Predicates.at("document.type", "article"), {
-//     // orderings: "[my.article.date desc]"
-//   });
+  const articles = await client.query(Prismic.Predicates.at("document.type", "article"), {
+    // orderings: "[my.article.date desc]"
+  });
 
-//   return {
-//     props: {
-//       articles: articles ? articles.results : []
-//     }
-//   };
-// }
+  console.log('articles prismic', articles);
+  return {
+    props: {
+      articles: articles ? articles.results : []
+    }
+  };
+}
 
 export default Artiles;
