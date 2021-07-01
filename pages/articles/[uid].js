f53b5b23726f78d565e72c0e57431ffe58f03240
Kosuke Sato <kosuke.sato030@gmail.com>
2021-07-01 19:15:24 +0900

    fix conflict

--- pages/articles/[uid].js
+++ pages/articles/[uid].js
@@ -15,7 +15,9 @@ import {
 import { useRouter } from "next/router";
 import { TwitterShareButton, FacebookShareButton } from "react-share";
 
-const Article = ({ doc, publishDate }) => {
+const Article = ({ doc }) => {
+  console.log("docdocdoc", doc);
+  console.log("docdocdoc", doc);
   const [pickUpArticles, setPicUpArticles] = useState([]);
   const apiEndpoint = process.env.NEXT_PUBLIC_PRISMIC_API_END_POINT;
   const router = useRouter();
